e570596b8a726f4a365a50547f567084cc69705e
Adalberto Hofmann <adalberto@css4html.com.br>
2019-10-08 12:33:17 -0300

    [master]Menu adjustments and social card style

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,27 @@
     <div class="bd-gray">
       <div class="ml-container ml-header">
         <div class="ml-header-time"><span>23/09/2019 10:56:59</span></div>
-        <div class="ml-header-my-account-link"><span>MINHA CONTA <a href="javascript:;">v</a></span></div>
-        <div class="ml-header-engine"><span>*</span></div>
+        <div class="ml-header-my-account-link"><a href="">MINHA CONTA<i class="ml-header-my-account-link-down"></i></a></div>
+        <div class="ml-header-engine"><div class="ml-header-engine-config"></div></div>
+      </div>
+    </div>
+    <div class="bB">
+      <div class="ml-container ml-menu">
+        <div class="ml-menu-logo">
+          <a href=""><div><img src="./assets/images/logotipo.png" title="mLabs"></div></a>
+        </div>
+        <nav class="ml-menu-nav"> 
+          <ul>
+            <li><a href=""><div><img src="./assets/images/dashboard.svg"><p>DASHBOARD</p></div></a></li>
+            <li class="ml-new"><a href=""><div><img src="./assets/images/agendamento.svg"><p>AGENDAR POST</p></div></a></li>
+            <li><a href=""><div><img src="./assets/images/calendar.svg"><p>CALENDÁRIO</p></div></a></li>
+            <li><a href=""><div><img src="./assets/images/inbox.svg"><p>INBOX</p></div></a></li>
+            <li><a href=""><div><img src="./assets/images/news.png"><p>FEED</p></div></a></li>
+            <li><a href=""><div><img src="./assets/images/conteudo.svg"><p>WORKFLOW</p></div></a></li>
+            <li><a href=""><div><img src="./assets/images/acompanhamento.svg"><p>ACOMPANHAMENTO</p></div></a></li>
+            <li><a href=""><div><img src="./assets/images/relatorios.svg"><p>RELATÓRIOS</p></div></a></li>
+          </ul>
+        </nav>
       </div>
     </div>
     <div>
@@ -14,6 +33,14 @@
 </template>
 <style lang="sass">
 @import url('https://fonts.googleapis.com/css?family=Exo&display=swap');
+@import url("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+
+@font-face
+  font-family: 'prime'
+  src: local('prime'), url('./assets/fonts/prime_regular-webfont.ttf') format('ttf')
+  src: local('prime'), url('./assets/fonts/prime_regular-webfont.woff') format('woff')
+  font-weight: normal
+  font-style: normal
 
 *
   padding: 0
@@ -24,7 +51,7 @@ html, body
   height: 100vh
 
 body
-  font-family: 'Exo', sans-serif
+  font-family: 'prime', sans-serif
 
 .bd-gray
   background-color: #3a3a3a
@@ -33,11 +60,11 @@ body
   background-color: #f7f7f7
 
 .bB
-  border-bottom: 1.5px solid #d2d3d3
+  border-bottom: 1px solid #d2d3d3
 
 .ml-container
   margin: 0 auto
-  max-width: 1190px
+  max-width: 1200px
 
 .ml-header
   height: 32px
@@ -52,23 +79,140 @@ body
   &-engine
     width: 33%
     padding: 0 15px
+
+  &-time
+    font-family: Arial, Helvetica, sans-serif
+    font-size: 12px
+    color: #cbcccd
  
   &-my-account-link
     text-align: center
 
     a
-      font-size: 9px
+      font-size: 12px
       text-decoration: none
-      color: #fff
+      color: #cbcccd
       position: relative
-      bottom: 3px
       padding: 0 10px
+
+  &-my-account-link-down
+    margin-left: 10px
+    font-size: 16px
+    font: normal normal normal 14px/1 FontAwesome
+
+    &::before
+      content: "\f107"
 
   &-engine
     text-align: right
-    font-size: 32px
-    top: 5px
-    position: relative
+    display: flex
+    justify-content: flex-end
 
-    
+    &-config
+      width: 16px;
+      height: 16px;
+      background: url(./assets/images/icons.png) no-repeat -32px -121px;
+      position: relative;
+      z-index: 10;
+
+.ml-menu
+  display: flex
+  align-items: flex-end
+  justify-content: space-between
+  flex-wrap: wrap
+
+  &-logo
+    flex: 1
+
+    div
+      display: flex
+
+      img
+        margin: 0 auto
+        padding-bottom: 10px
+
+  &-nav ul
+    display: flex
+    justify-content: center
+    flex-wrap: wrap
+    list-style: none
+
+    li
+      text-align: center
+      min-width: auto
+      height: 90px
+      display: flex
+      justify-content: center
+      align-items: flex-end
+      padding: 15px 16px 10px
+
+      img
+        opacity: 0.5
+        -webkit-transition: 0.3s all ease-in
+        transition: 0.3s all ease-in
+
+      a
+        text-decoration: none
+        color: #535353
+        // opacity: 0.5
+        -webkit-transition: 0.3s all ease-in
+        transition: 0.3s all ease-in
+
+        &:hover
+          opacity: 1
+          color: #e02b4a
+
+          img
+            opacity: 1
+
+    .ml-new
+      background: url(./assets/images/novo.png) no-repeat 95px 15px
+
+.ml-content
+  padding: 40px 0
+
+  ul
+    display: flex
+    flex-wrap: wrap
+    list-style: none
+
+    li
+      text-align: center
+      min-width: 222px
+      min-height: 218px
+      border: dotted 2px #d2d3d3
+      background-color: #eff0f0
+      padding: 30px
+      display: flex
+      align-items: center
+      flex-direction: column
+      justify-content: center
+
+      img.social-logo
+        width: 57px
+        height: 57px
+
+      img.social-question
+        width: 25px
+        height: 25px
+        position: relative
+        left: 60px
+        top: 30px
+
+      p
+        font-size: 23px
+        color: #535353
+        padding-top: 15px
+
+
+      button
+        width: 90px
+        height: 30px
+        margin-top: 22px
+        background-color: #de2e4d
+        color: #fff
+        font-size: 15px
+        border: none
+        border-radius: 5px
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="home">
+  <!-- <div class="home">
     <h1>This is an home page</h1>
-  </div>
+  </div> -->
+  <section class="bg-whitesmoke">
+      <div class="ml-container ml-content">
+        <ul class="ml-social">
+          <li class="ml-social-network">
+            <img class="social-question" src="../assets/images/question.png">
+            <img class="social-logo" src="../assets/images/facebook.png">
+            <p>FACEBOOK</p>
+            <button>Adicionar</button>
+          </li>
+        </ul>
+      </div>
+    </section>
 </template>
