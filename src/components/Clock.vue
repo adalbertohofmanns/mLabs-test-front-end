14c6f394913d0b332133c907cf9544053872e8b1
Adalberto Hofmann <adalberto.hofmann@mlabs.com.br>
2021-02-16 16:53:16 -0300

    ajustando imagens

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,3 +1,4 @@
+/* eslint-disable */
 <template>
   <span> {{ dateTimeString }} </span>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,4 @@
+/* eslint-disable */
 <template>
   <section class="bg-whitesmoke">
     <div class="ml-container ml-content">
